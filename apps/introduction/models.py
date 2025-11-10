5365ab956a0ab084777e7295215da591e590a835
--global <ghasemisppc@gmail.com>
2025-11-10 21:11:06 +0330

    1404-08-19-3

--- apps/introduction/models.py
+++ apps/introduction/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 #-----------------------------------------------------------------------------------------
-class Visitor_group(models.Model):
+class Visitor_Group(models.Model):
     code = models.PositiveIntegerField(max_length=10 ,help_text='کد یکتا برای گروه',verbose_name='کد ')
     TITLE_CHOICES = [
         ('1' , 'عمومی'),
@@ -18,3 +18,34 @@ class Visitor_group(models.Model):
 class Place (models.Model):
     code = models.CharField(max_length=50 , unique=True , help_text='کد یکتا برای مکان',verbose_name='کد')
     title = models.CharField(max_length=50 , help_text='نام مکان',verbose_name='عنوان')
+    main_image = models.ImageField(upload_to='places/main_image', blank=True ,null=True)
+    visiting_hours = models.CharField(max_length=255 , blank=True , null=True , verbose_name='ساعت بازدید')
+    OPEN_DAY_CHOISES = [
+        ('mon', 'دوشنبه'),
+        ('tue', 'سه‌شنبه'),
+        ('wed', 'چهارشنبه'),
+        ('thu', 'پنج‌شنبه'),
+        ('fri', 'جمعه'),
+        ('sat', 'شنبه'),
+        ('sun', 'یک‌شنبه'),
+    ]
+    open_days = models.CharField(max_length=100, blank=True,null=True,verbose_name='روز بازدید')
+    regulations = models.TextField(blank=True , null=True ,help_text='فوانین و مقررات' ,verbose_name='قوانین')
+    more_info = models.TextField(blank=True,null=True,help_text='اطلاعت بیشتر',verbose_name='اطلاعات بیشتر')
+    created_at = models.DateTimeField(auto_now_add=True,verbose_name='تاریخ ثبت مکان')
+    
+    def __str__(self):
+        return self.code + '' + self.title
+    
+#------------------------------------------------------------------------------------------
+class Ticket_Price(models.Model):
+    code = models.CharField(max_length=50 , unique=True , help_text='کد یکتا برای بلیط',verbose_name='کد')
+    visitor_group = models.ForeignKey(Visitor_Group,on_delete=models.CASCADE , related_name='ticket_price')
+    place = models.ForeignKey(Place , on_delete=models.CASCADE,related_name='ticket_price')
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+
+    class Meta :
+        unique_together = ('visitor_group','place')
+
+    def __str__(self):
+        return self.code
