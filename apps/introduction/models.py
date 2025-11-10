364fb4dda0f52a2044c5da2b71641df528f530f5
--global <ghasemisppc@gmail.com>
2025-11-10 20:10:10 +0330

    1404-08-19-2

--- apps/introduction/models.py
+++ apps/introduction/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+#-----------------------------------------------------------------------------------------
+class Visitor_group(models.Model):
+    code = models.PositiveIntegerField(max_length=10 ,help_text='کد یکتا برای گروه',verbose_name='کد ')
+    TITLE_CHOICES = [
+        ('1' , 'عمومی'),
+        ('2' , 'گروهی'),
+        ('3' , 'دانشجویی'),
+        ('4' , 'خارجی'),
+    ]
+    title = models.CharField(max_length=10,choices=TITLE_CHOICES,default='1',verbose_name='عنوان')
+
+    def __str__(self):
+        return self.code + ' ' + self.title
+
+#------------------------------------------------------------------------------------------
+class Place (models.Model):
+    code = models.CharField(max_length=50 , unique=True , help_text='کد یکتا برای مکان',verbose_name='کد')
+    title = models.CharField(max_length=50 , help_text='نام مکان',verbose_name='عنوان')
