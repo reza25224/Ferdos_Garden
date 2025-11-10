555bdd92082ba66b7fca274405d58bb43c62ce21
--global <ghasemisppc@gmail.com>
2025-11-10 13:45:16 +0330

    1404-08-19

---
+++ apps/introduction/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+   path('step1/',views.step1)
+
+]
---
+++ config/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from django.urls import path , include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include('apps.main.urls')),
+    path('introduction',include('apps.introduction.urls')),
+]
